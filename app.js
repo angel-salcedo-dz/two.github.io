b1c2d1cfc22b3986251b2d89fd175bae5269ac5b
Angel Salcedo <sem.xero@gmail.com>
2025-10-15 01:05:24 -0600

    Push

--- app.js
+++ app.js
@@ -1,5 +1,14 @@
 import Alpine from 'alpinejs'
-
 window.Alpine = Alpine
 
 Alpine.start()
+
+import { gsap } from "gsap";
+import { ScrollTrigger } from "gsap/ScrollTrigger";
+import { ScrollSmoother } from "gsap/ScrollSmoother";
+
+gsap.registerPlugin(ScrollTrigger, ScrollSmoother);
+window.gsap = gsap;
+window.ScrollTrigger = ScrollTrigger;
+window.ScrollSmoother = ScrollSmoother;
+
